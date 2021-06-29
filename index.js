d82ee2391d659948a249272fd43b522eccd3e871
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-06-29 14:58:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var winner = document.querySelector("h1");
 
 // player1 dice
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelectorAll("img")[0].setAttribute("src", "dice" + randomNumber1 + ".png");
 
 // player2 dice
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.querySelectorAll("img")[1].setAttribute("src", "dice" + randomNumber2 + ".png");
 
 // checks who is the winner
 if(randomNumber1 > randomNumber2){
